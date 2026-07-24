c831b9f99f89b86e43a5292697f2adacfde85ad1
Philippe Stons <philippe.stons@bstorm.be>
2026-07-24 09:48:47 +0200

    [PATHLIB] import with pathlib

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -1,8 +1,13 @@
 import os
 import glob
+from pathlib import Path
 
-print(os.path.dirname(__file__))
-print(os.path.dirname(__file__) + "/*.py")
-print(glob.glob(os.path.dirname(__file__) + "/*.py"))
-print([os.path.basename(f)[:-3] for f in glob.glob(os.path.dirname(__file__) + "/*.py")])
-__all__ = [os.path.basename(f)[:-3] for f in glob.glob(os.path.dirname(__file__) + "/*.py")]
+# print(os.path.dirname(__file__))
+# print(os.path.dirname(__file__) + "/*.py")
+# print(glob.glob(os.path.dirname(__file__) + "/*.py"))
+# print([os.path.basename(f)[:-3] for f in glob.glob(os.path.dirname(__file__) + "/*.py")])
+# __all__ = [os.path.basename(f)[:-3] for f in glob.glob(os.path.dirname(__file__) + "/*.py")]
+
+# Avec pathlib
+path = Path(__file__).parent.absolute()
+__all__ = [f.name[:-3] for f in path.iterdir() if f.is_file() and f.name.endswith(".py")]
